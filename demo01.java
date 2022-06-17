948a995ca2685e6009dc264960fc64929cf40b41
白昼 <74648359+baizhouer@users.noreply.github.com>
2022-06-17 14:16:40 +0800

    修改此文件

--- demo01.java
+++ demo01.java
@@ -4,6 +4,7 @@ public class demo01 {
     public static void main(String[] args) {
         int[] arr={1,3,5,6};
         System.out.println(searchInsert(arr,2));
+        //sout
 
     }
     // 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
